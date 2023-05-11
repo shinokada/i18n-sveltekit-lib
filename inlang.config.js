ce70ca257202a8105e7e72f9536d0042e2ad3351
Shinichi Okada <okada.shin@gmail.com>
2023-05-12 06:07:17 +0900

    feat: update inlang sdk

--- inlang.config.js
+++ inlang.config.js
@@ -7,8 +7,11 @@ export async function defineConfig(env) {
 		"https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2.2.0/dist/index.js"
 	)
 	const { default: sdkPlugin } = await env.$import(
-		"https://cdn.jsdelivr.net/npm/@inlang/sdk-js-plugin@0.3.1/dist/index.js"
+		"https://cdn.jsdelivr.net/npm/@inlang/sdk-js-plugin@0.4.4/dist/index.js"
 	)
+	// const { default: pluginJson } = await env.$import(
+	// 	"https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js",
+	// )
 
 	return {
 		referenceLanguage: "en",
